0edf47742cdc817230516fdc4160f0b18fd5d0c3
e-young <duddl4766@gmail.com>
2022-03-16 17:59:08 +0900

    components

--- scripts/script.js
+++ scripts/script.js
@@ -47,13 +47,30 @@ $(document).click(function (e) {
 
   // <<--------- input ------------
 
+    // --------- select ------------>>
 
-  $(".label").click(function () {
-    const select = $(this);
+  /* 셀렉트박스 보이게 하기 */
 
-    // 드롭다운 열기
-    select.next(".optionList").slideDown(100);
-
-   
+  $("body").on("click", ".label", function () {
+    $(".optionList").toggle();
   });
+
+  /* 셀렉트 박스 옵션 선택 */
+  $("body").on("click", "ul li", function () {
+    var text = $(this).html();
+    $(".label").html(text);
+    $("ul").toggle();
+  });
+
+  /* 셀렉트 박스 이외 선택시 보이지 않게 하기 */
+  $("body").on("click", function (e) {
+    if ($("ul").css("display") == "block") {
+      if ($(".selectBox").has(e.target).length == 0) {
+        $("ul").hide();
+      }
+    }
+  });
+
+  // <<--------- select ------------
+  
 });
