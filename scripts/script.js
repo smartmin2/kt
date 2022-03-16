e55eda28059854880ccba01419b8bd49c29e10a2
e-young <duddl4766@gmail.com>
2022-03-16 16:28:54 +0900

    login

---
+++ scripts/script.js
@@ -0,0 +1,49 @@
+$(function () {
+  // --------- input ------------>>
+
+  $("input").focusin("input", function () {
+    const btnClear = $(this).parent(".input-wrap").find(".btn-clear");
+    if ($(this).val() == "") {
+      btnClear.hide();
+      $(this).css("background-size", "inherit");
+    } else {
+      btnClear.show();
+      $(this).css("background-size", "0%");
+    }
+  });
+
+  // $("input").focusout("input", function () {
+  //   const btnClear = $(this).parent(".input-wrap").find(".btn-clear");
+  //   btnClear.hide();
+  //   $(this).css("background-size", "inherit");
+  // });
+
+  $("input").on("input", function () {
+    const btnClear = $(this).parent(".input-wrap").find(".btn-clear");
+    if ($(this).val() == "") {
+      btnClear.hide();
+      $(this).css("background-size", "inherit");
+    } else {
+      btnClear.show();
+      $(this).css("background-size", "0%");
+    }
+  });
+
+  $(".btn-clear").on("click", function () {
+    $(this).parent(".input-wrap").find("input").val("");
+    const btnClear = $(this).parent(".input-wrap").find(".btn-clear");
+    btnClear.hide();
+    $(this).parent(".input-wrap").find("input").css("background-size", "inherit");
+  });
+});
+
+$(document).click(function (e) {
+  var LayerPopup = $(".input-wrap");
+  if (LayerPopup.has(e.target).length === 0) {
+    const btnClear = $(this).find(".btn-clear");
+    btnClear.hide();
+    $(this).find("input").css("background-size", "inherit");
+  }
+
+  // <<--------- input ------------
+});
